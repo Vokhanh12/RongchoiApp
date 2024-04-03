4bb7971ad1bf0d2f677e9c84ca90fa37d66c1c45
khanh <khanhyou2018@gmail.com>
2024-04-03 19:46:07 +0700

    add commit

--- lib/data/repositories/data_authentication_repository.dart
+++ lib/data/repositories/data_authentication_repository.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:rongchoi_app/firebase_options.dart';
 import 'package:logging/logging.dart';
 
-import 'package:rongchoi_app/domain/entities/user.dart' as user_en;
+import 'package:rongchoi_app/domain/entities/user.dart' as en;
 
 class DataAuthenticationRepository extends AuthenticationRepository {
   // Members
@@ -34,6 +34,8 @@ class DataAuthenticationRepository extends AuthenticationRepository {
     Uri url = Uri.parse("http://10.0.2.2:8080/v1/login");
     UserCredential userCredential;
     Map<String, dynamic> body;
+
+    // handle firebase
     try {
       // Initialize Firebase
       await Firebase.initializeApp(
@@ -42,7 +44,7 @@ class DataAuthenticationRepository extends AuthenticationRepository {
       // Sign with email and password to Firebase
       userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
           email: 'test@gmail.com', password: 'Aa@123456789');
-      _logger.finest('Login Successful.');
+      _logger.finest('Login firebase Successful.');
     } on FirebaseAuthException catch (ex) {
       Log.d("something bad happened runtimeType:ex.runtimeType", runtimeType);
       Log.d("$ex", runtimeType);
@@ -51,33 +53,23 @@ class DataAuthenticationRepository extends AuthenticationRepository {
       rethrow;
     }
 
+    // handle server
     try {
       // If user credentials are successful, then retrieve the token from Firebase.
       var token = await userCredential.user!.getIdToken();
 
-      Log.d("Bearer $token ", runtimeType);
-
       Map<String, String> query = {
-        'Authorization':
-            'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImJhNjI1OTZmNTJmNTJlZDQ0MDQ5Mzk2YmU3ZGYzNGQyYzY0ZjQ1M2UiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcm9uZ2Nob2ktZTk2OTAiLCJhdWQiOiJyb25nY2hvaS1lOTY5MCIsImF1dGhfdGltZSI6MTcxMjA2NDA2NiwidXNlcl9pZCI6IlZabjY2T3BFa2pNeXprSFNUbzJCcUNQWlU3RjMiLCJzdWIiOiJWWm42Nk9wRWtqTXl6a0hTVG8yQnFDUFpVN0YzIiwiaWF0IjoxNzEyMDY0MDY2LCJleHAiOjE3MTIwNjc2NjYsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdEBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.F3ypgu83NMkEJtmqzCYPGsTbHBw60YgV0LPe0j2dBaOPAP_yICtMCQuqRyoQQjO8sjbwninteeflEG_RGEvNUl9RVMz0kJ-FsNC12K90V9ObqM0gvhJHh1jeJ9lWz3KSjjSRrFIYZypawNbqErSOR77ArYTru5ubq3ynoJKmOiuugQVsT_HAIoXHFVioJR0xciaf1lLk-Tx2Kx5qVP1Haw9avCTOJ5ro8QyDtSMoA9QZ0gFqMeCzk2iP9GGUfrrjrPqLtd9GqoPdF9AxNzjUCgGrxMGjvwksICkgBVWXuJkYyqOhQC_4tzejvR4ZPkXwRchf902oDG54OKUDntWZJA',
+        'Authorization': 'Bearer $token',
       };
-      print('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
       // Invoke http request to login and convert body to map
-      try {
-        print("query:" + query.toString());
-        print("url:" + url.toString());
-        body =
-            await HttpHelper.invokeHttp(url, RequestType.get, headers: query);
-        user_en.User user = user_en.User.fromJson(body);
-        print(user.toJson());
-      } catch (e) {
-        print('Xảy ra lỗi khi gọi API: $e');
-      }
+      body = await HttpHelper.invokeHttp(url, RequestType.get, headers: query);
+      
+      // en = entity package
+      en.User user = en.User.fromJson(body);
+      print('getUser Successful. ${user.toJson()}');
     } catch (ex) {
       print('bug:$ex');
     }
-
-    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
   }
 
   @override
--- lib/data/utils/http_helper.dart
+++ lib/data/utils/http_helper.dart
@@ -13,11 +13,13 @@ class HttpHelper {
   /// [body] and [encoding] only apply to [RequestType.post] and [RequestType.put] requests. Otherwise,
   /// they have no effect.
   /// This is optimized for requests that anticipate a response body of type `Map<String, dynamic>`, as in a json file-type response.
-  static Future<Map<String, dynamic>> invokeHttp(dynamic url, RequestType type, {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
+  static Future<Map<String, dynamic>> invokeHttp(dynamic url, RequestType type,
+      {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
     http.Response response;
     Map<String, dynamic> responseBody;
     try {
-      response = await _invoke(url, type, headers: headers, body: body, encoding: encoding);
+      response = await _invoke(url, type,
+          headers: headers, body: body, encoding: encoding);
     } catch (error) {
       rethrow;
     }
@@ -31,11 +33,13 @@ class HttpHelper {
   /// [body] and [encoding] only apply to [RequestType.post] and [RequestType.put] requests. Otherwise,
   /// they have no effect.
   /// This is optimized for requests that anticipate a response body of type `List<dynamic>`, as in a list of json objects.
-  static Future<List<dynamic>> invokeHttp2(dynamic url, RequestType type, {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
+  static Future<List<dynamic>> invokeHttp2(dynamic url, RequestType type,
+      {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
     http.Response response;
     List<dynamic> responseBody;
     try {
-      response = await _invoke(url, type, headers: headers, body: body, encoding: encoding);
+      response = await _invoke(url, type,
+          headers: headers, body: body, encoding: encoding);
     } on APIException {
       rethrow;
     } on SocketException {
@@ -47,11 +51,13 @@ class HttpHelper {
   }
 
   /// Invoke the `http` request, returning the [http.Response] unparsed.
-  static Future<http.Response> _invoke(dynamic url, RequestType type, {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
+  static Future<http.Response> _invoke(dynamic url, RequestType type,
+      {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
     http.Response response;
 
     try {
-      final usedEncoding = encoding ?? utf8; // Use utf8 as default encoding if encoding is null
+      final usedEncoding =
+          encoding ?? utf8; // Use utf8 as default encoding if encoding is null
 
       switch (type) {
         case RequestType.get:
@@ -83,7 +89,7 @@ class HttpHelper {
       rethrow;
 
       // Handle no internet connection
-    } on SocketException catch(e) {
+    } on SocketException catch (e) {
       throw Exception(e.osError?.message);
     } catch (error) {
       rethrow;
@@ -95,19 +101,17 @@ class HttpHelper {
 enum RequestType { get, post, put, delete }
 
 void main() async {
-
-   Uri url = Uri.parse("http://10.0.2.2:8080/v1/login");
+  Uri url = Uri.parse("http://10.0.2.2:8080/v1/users");
 
   Map<String, String> query = {
     'Authorization':
-        'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImJhNjI1OTZmNTJmNTJlZDQ0MDQ5Mzk2YmU3ZGYzNGQyYzY0ZjQ1M2UiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcm9uZ2Nob2ktZTk2OTAiLCJhdWQiOiJyb25nY2hvaS1lOTY5MCIsImF1dGhfdGltZSI6MTcxMjA2NDA2NiwidXNlcl9pZCI6IlZabjY2T3BFa2pNeXprSFNUbzJCcUNQWlU3RjMiLCJzdWIiOiJWWm42Nk9wRWtqTXl6a0hTVG8yQnFDUFpVN0YzIiwiaWF0IjoxNzEyMDY0MDY2LCJleHAiOjE3MTIwNjc2NjYsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdEBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.F3ypgu83NMkEJtmqzCYPGsTbHBw60YgV0LPe0j2dBaOPAP_yICtMCQuqRyoQQjO8sjbwninteeflEG_RGEvNUl9RVMz0kJ-FsNC12K90V9ObqM0gvhJHh1jeJ9lWz3KSjjSRrFIYZypawNbqErSOR77ArYTru5ubq3ynoJKmOiuugQVsT_HAIoXHFVioJR0xciaf1lLk-Tx2Kx5qVP1Haw9avCTOJ5ro8QyDtSMoA9QZ0gFqMeCzk2iP9GGUfrrjrPqLtd9GqoPdF9AxNzjUCgGrxMGjvwksICkgBVWXuJkYyqOhQC_4tzejvR4ZPkXwRchf902oDG54OKUDntWZJA',
+        'ApiKey d730b03c35485ac408979881484d386edf78a7855d64596ebac5feb9b86e0165',
   };
 
   try {
     // Invoke http request to login and convert body to map
-    Map<String, dynamic> body = await HttpHelper.invokeHttp(
-        url, RequestType.get,
-        headers: query);
+    Map<String, dynamic> body =
+        await HttpHelper.invokeHttp(url, RequestType.get, headers: query);
     print('getUser Successful.');
   } catch (error) {
     print(error);
